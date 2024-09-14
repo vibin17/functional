c1a2619554f2de727e2c1e2235c4563358107d31
DanilaGrachev <danyagrachev@gmail.com>
2024-09-14 14:22:22 +0600

    formatting

--- Lab1/Program.fs
+++ Lab1/Program.fs
@@ -3,12 +3,12 @@ open System.IO;
 open System.Text.RegularExpressions;
 
 let mostFrequentGroup =
-    ((String.Empty, File.ReadAllLines @"text.txt") |> String.Join, "\w+") |> 
-    Regex.Matches |> 
-    Seq.map(fun a -> a.Value) |> 
-    Seq.countBy(fun word -> word.ToLower()) |> 
-    Seq.groupBy(fun (_, frequency) -> frequency) |>
-    Seq.maxBy(fun group -> fst group)
+    ((String.Empty, File.ReadAllLines @"text.txt") |> String.Join, "\w+")
+    |> Regex.Matches
+    |> Seq.map(fun a -> a.Value)
+    |> Seq.countBy(fun word -> word.ToLower())
+    |> Seq.groupBy(fun (_, frequency) -> frequency)
+    |> Seq.maxBy(fun group -> fst group)
 
 for wordFrequencyPair in snd mostFrequentGroup do
     printfn "%A" wordFrequencyPair
