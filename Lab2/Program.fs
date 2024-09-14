3e5df7a96bec4635a6b53ca502f205a9c366f42a
DanilaGrachev <danyagrachev@gmail.com>
2024-09-14 14:47:15 +0600

    wip lab2

---
+++ Lab2/Program.fs
@@ -0,0 +1,19 @@
+﻿// For more information see https://aka.ms/fsharp-console-app
+open System;
+open System.IO;
+open System.Text.RegularExpressions
+
+let text = (String.Empty, File.ReadAllLines(@"text.txt")) |> String.Join
+
+let b = 
+    Regex.Matches(text, "\((\d+),[ ,]?(\d+)\)")
+    |> Seq.map(fun a ->
+        let x = Double.Parse a[1]
+        let y = Double.Parse a[2]
+        return (x, y))
+
+let a = 
+    ((String.Empty, File.ReadAllLines(@"text.txt")) |> String.Join, "\((\d+),[ ,]?(\d+)\)")
+    |> Regex.Matches; 
+
+return
