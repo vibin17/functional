0c6a9e8302126e4fcd67d1ec4d4869f6ebad2725
DanilaGrachev <danyagrachev@gmail.com>
2024-09-21 11:10:03 +0600

    lab4

--- Lab4/Program.fs
+++ Lab4/Program.fs
@@ -1,2 +1,15 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿// Task 1
+
+// For more information see https://aka.ms/fsharp-console-apps
+open System;
+
+printfn "Enter a number"
+
+let n = bigint.Parse(Console.ReadLine())
+let rec factorialRecursive (n: bigint) : bigint = 
+    if n = bigint.One then n
+    else n * factorialRecursive(n - bigint.One)
+
+let factorial = factorialRecursive (n)
+
+printfn "Factorial: %A" factorial
